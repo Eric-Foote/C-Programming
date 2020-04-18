d397cad0d83f8eba4b49458079b81401e76c1b38
clocknight <EricFoote2014@hotmail.com>
2020-04-18 13:34:04 -0300

    Put the body of main in with one of the example tests as way to test the logic of the count_sheep function

--- Programming Problems/counting-sheep/countingsheep.c
+++ Programming Problems/counting-sheep/countingsheep.c
@@ -20,3 +20,25 @@ Hint: Don't forget to check for bad values like null/undefined
 #include <stddef.h>
 
 size_t count_sheep(const bool *sheep, size_t count);
+
+void main()
+{
+    //going to use as input to the function the given array in the sample test 
+    const bool sheep[] = {
+      true,  true,  true,  false,
+      true,  true,  true,  true,
+      true,  false, true,  false,
+      true,  false, false, true,
+      true,  true,  true,  true,
+      false, false, true,  true
+    };
+    // I am going to use size_t count as the size of the array given 
+    size_t count  = sizeof(sheep);
+    size_t countOfSheep = count_sheep(&sheep, count);
+    printf("The size of the array is %zd\n", countOfSheep);
+}
+
+size_t count_sheep(const bool *sheep, size_t count)
+{
+    
+}
