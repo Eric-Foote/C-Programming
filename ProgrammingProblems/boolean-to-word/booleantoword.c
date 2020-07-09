62a811ff20f21efd93b1b9f3fa708d8391415c30
clocknight <EricFoote2014@hotmail.com>
2020-07-09 12:43:33 -0300

    Redid the folders

---
+++ ProgrammingProblems/boolean-to-word/booleantoword.c
@@ -0,0 +1,17 @@
+/*
+ *Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ */
+#include<stdio.h>
+
+const char *bool_to_word (int value);
+
+void main()
+{
+    int value1= 0 != 0
+    int value2 = 0 == 0;
+}
+
+const char *bool_to_word(int value)
+{
+    
+}
---
+++ ProgrammingProblems/counting-sheep/countingsheep.c
@@ -0,0 +1,50 @@
+/*
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+{ true,  true,  true,  false,
+  true,  true,  true,  true,
+  true,  false, true,  false,
+  true,  false, false, true,
+  true,  true,  true,  true,
+  false, false, true,  true }
+
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+
+*/
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+
+size_t count_sheep(const bool *sheep, size_t count);
+
+void main()
+{
+    //going to use as input to the function the given array in the sample test 
+    const bool sheep[] = {
+      true,  true,  true,  false,
+      true,  true,  true,  true,
+      true,  false, true,  false,
+      true,  false, false, true,
+      true,  true,  true,  true,
+      false, false, true,  true
+    };
+    // I am going to use size_t count as the size of the array given 
+    size_t count = sizeof(sheep) / sizeof(sheep[0]);
+    size_t countOfSheep = count_sheep(sheep, count);
+    printf("The size of the array is %zd\n", countOfSheep);
+}
+
+size_t count_sheep(const bool *sheep, size_t count) 
+{
+    size_t counter;
+    for(int i = 0; i < (int)count; i++){
+        if(sheep[i])
+            counter++;
+    }
+    return counter;
+}
