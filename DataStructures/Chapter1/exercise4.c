f3069d82a237cdc8ff8fd67806904df07c92050a
clocknight <EricFoote2014@hotmail.com>
2020-07-10 11:01:47 -0300

    Wrote out the next two exercises

---
+++ DataStructures/Chapter1/exercise4.c
@@ -0,0 +1,1 @@
+// Write a program to read the address of a user. Display the result by breaking it into multiple lines
---
+++ DataStructures/Chapter1/exercise5.c
@@ -0,0 +1,1 @@
+// Write a program to read two floating point numbers. Add these numbers and assign the result to an integer. Finally display the value of all three variables.
