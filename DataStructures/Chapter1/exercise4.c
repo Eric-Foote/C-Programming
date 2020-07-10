5aa17d9f194e6cf5032defd767824f5322d8461b
clocknight <EricFoote2014@hotmail.com>
2020-07-10 20:53:06 -0300

    I think this is what the problem wanted

--- DataStructures/Chapter1/exercise4.c
+++ DataStructures/Chapter1/exercise4.c
@@ -1,1 +1,15 @@
 // Write a program to read the address of a user. Display the result by breaking it into multiple lines
+
+
+#include <stdio.h>
+
+int main()
+{	
+	int i;
+	printf("Give me an integer");
+	scanf("%d",&i);
+	int *a = &i;
+	printf("%d is the address of what you gave me", &a);
+	return 0;
+}
+
